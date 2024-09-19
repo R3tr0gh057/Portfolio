1c95c462e53526d4179316439f3a4dc7d8b9e5dc
Joe Sanjo <dedsec8.0.8.0x00@gmail.com>
2024-09-20 01:46:48 +0530

    Completed loading page
    - added gsap animated loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,37 @@
 import LoadingPage from "./loadingPage";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 function App() {
+  useGSAP(()=>{
+    let t1 = gsap.timeline();
+    t1.to(".box", {
+      scale:0,
+      y:60,
+      rotate: 400,
+      duration: 1,
+      repeat: 1,
+      yoyo: true,
+      delay: 0.5,
+      stagger: {
+        amount: 1.5,
+        from: "start",
+        // axis: "x",
+        grid: [3, 3],
+      },
+    });
+
+    t1.to(".container", {
+      rotate: "-405deg",
+      scale: 0,
+      duration: 1,
+    });
+    t1.to(".wrapper", {
+      opacity: 0,
+      duration: 1,
+      delay: 2,
+    })
+  });
   return (
     <>
       <LoadingPage />
